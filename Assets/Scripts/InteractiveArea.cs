4f27fb495c99d2f4e9067d08750c9c64add1418f
SofiaS21 <49876490@est.ort.edu.ar>
2026-06-01 10:47:11 -0300

    Clase 2

---
+++ Assets/Scripts/InteractiveArea.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractiveArea : MonoBehaviour
+{
+
+    public UIManager uiManager;
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.CompareTag("Collectable"))
+        {
+            Debug.Log("Hubo colision con un Collectable");
+            Destroy(col.gameObject);
+            uiManager.AddPointAndUpdateuiManager();
+        }
+    }
+}
