d68900523d444258e4205fea8473f6cb88e5053d
SofiaS21 <49876490@est.ort.edu.ar>
2026-06-29 12:10:47 -0300

    para que gire unos scripts

---
+++ Assets/Scripts/PreviewManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PreviewManager : MonoBehaviour
+{
+    public Transform spawnPreview;
+    private GameObject objetoActual;
+
+        public void MostrarObjeto(GameObject prefab)
+        {
+            if (objetoActual != null)
+            {
+                Destroy(objetoActual);
+            }
+
+            objetoActual = Instantiate(prefab, spawnPreview.position, Quaternion.identity);
+
+            objetoActual.transform.SetParent(spawnPreview);
+
+            objetoActual.AddComponent<RotarPreview>();
+        }
+}
---
+++ Assets/Scripts/RotarPreview.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotarPreview : MonoBehaviour
+{
+
+    void Update()
+        {
+            transform.Rotate(0, 60 * Time.deltaTime, 0);
+        }
+}
