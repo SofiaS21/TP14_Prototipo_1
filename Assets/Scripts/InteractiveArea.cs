1332212db45616b796b6e2e4b94e81c56904f9f8
SofiaS21 <49876490@est.ort.edu.ar>
2026-06-29 17:30:08 -0300

    Arregladisimo

--- Assets/Scripts/InteractiveArea.cs
+++ Assets/Scripts/InteractiveArea.cs
@@ -2,51 +2,66 @@
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
-
+ 
 public class InteractiveArea : MonoBehaviour
-{
-
+{ 
     UIManager uiManagerScript;
     GameManager gameManagerScript;
-    PreviewManager previewManager;
-    
-    public GameObject gameWonPanel; 
+    PreviewManager previewManager; 
+ 
+    public GameObject gameWonPanel;
     int score = 0;
-
-    void Awake ()
-
+ 
+    void Awake()
     {
         uiManagerScript = FindObjectOfType<UIManager>();
         gameManagerScript = FindObjectOfType<GameManager>();
-        previewManager = FindObjectOfType<PreviewManager>();
+        previewManager = FindObjectOfType<PreviewManager>(); 
     }
-
+ 
     void OnTriggerEnter(Collider other)
     {
         Collectable objeto = other.GetComponent<Collectable>();
-
-            Debug.Log("Entró al Collectable");
-
-if (objeto == null)
-{
-    Debug.Log("OBJETO ES NULL");
-}
-else
-{
-    Debug.Log("Preview: " + objeto.previewPrefab);
-
-    previewManager.MostrarObjeto(objeto.previewPrefab);
-}
-
-
-                if (score >= 14)
-                {
-                    uiManagerScript.ShowWonPanel();
-
-                    gameManagerScript.juegoTerminado = true;
-
-                    Time.timeScale = 0;
-                }
+ 
+        if (other.CompareTag("Collectable"))
+        {
+            score++;
+ 
+            MostrarPreviewSeguro(objeto, other.gameObject.name); 
+ 
+            uiManagerScript.UpdateScore(score);
+            Destroy(other.gameObject);
+        }
+ 
+        if (other.CompareTag("DoubleCollectable"))
+        {
+            score += 2;
+ 
+            MostrarPreviewSeguro(objeto, other.gameObject.name); 
+ 
+            uiManagerScript.UpdateScore(score);
+            Destroy(other.gameObject);
+        }
+ 
+        if (score >= 14)
+        {
+            uiManagerScript.ShowWonPanel();
+            gameManagerScript.juegoTerminado = true;
+            Time.timeScale = 0;
+        }
+    }
+ 
+    void MostrarPreviewSeguro(Collectable objeto, string nombre)
+    {
+        if (objeto == null) return;
+ 
+        if (objeto.previewPrefab != null)
+        {
+            previewManager.MostrarObjeto(objeto.previewPrefab);
+        }
+        else
+        {
+            Debug.LogWarning("Falta previewPrefab en: " + nombre);
+        }
     }
 }
-
